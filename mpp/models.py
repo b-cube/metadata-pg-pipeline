efb7b3bad15fe33bfaed5acd1955547d7f55d5ca
roomthily <sorenscott@gmail.com>
2015-09-25 22:50:02 -0600

    running with the base implementation

--- mpp/models.py
+++ mpp/models.py
@@ -1,45 +1,43 @@
 import sqlalchemy as sqla
-from sqlalchemy.orm import sessionmaker, mapper
-from sqlalchemy import Table, MetaData, Column, String, DateTime, Integer, Boolean
+from sqlalchemy import (
+    MetaData,
+    Column,
+    String,
+    DateTime,
+    Integer,
+    Boolean,
+    ForeignKey,
+)
 from sqlalchemy.dialects.postgresql import *
 from sqlalchemy.orm import relationship, backref
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
-metadata = sqla.MetaData()
-# Base = declarative_base()
+metadata = MetaData()
+Base = declarative_base()
 
 
-responses_table = Table(
-    'responses',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('source_url_sha', String(100)),
-    Column('source_url', String),
-    Column('xml_content_as_str', TEXT),
-    Column('initial_harvest_date', DateTime),
-    Column('raw_content', String),
-    Column('raw_content_md5', String),
-    Column('inlinks', ARRAY(String)),
-    Column('outlinks', ARRAY(String)),
-    Column('host', String),
-    Column('headers', ARRAY(String))
-)
+class Response(Base):
+    __tablename__ = 'responses'
+    id = Column(Integer, primary_key=True)
+    source_url_sha = Column(String(100))
+    source_url = Column(String)
+    xml_content_as_str = Column(TEXT)
+    initial_harvest_date = Column(DateTime)
+    raw_content = Column(String)
+    raw_content_md5 = Column(String)
+    inlinks = Column(ARRAY(String))
+    outlinks = Column(ARRAY(String))
+    host = Column(String)
+    headers = Column(ARRAY(String))
+
+    validations = relationship('Validation', backref='response')
 
 
-class Response(object):
-    def __init__(self, sha_id):
-        self.source_url_sha = sha_id
-
-validations_table = Table(
-    'validations',
-    metadata,
-    Column('id', Integer, primary_key=True),
-    Column('validated_on', DateTime),
-    Column('errors', ARRAY(String)),
-    Column('valid', Boolean),
-    Column('response_id', Integer)
-)
-
-
-mapper(Response, responses_table)
+class Validation(Base):
+    __tablename__ = 'validations'
+    id = Column(Integer, primary_key=True)
+    validated_on = Column(DateTime)
+    errors = Column(ARRAY(String))
+    valid = Column(Boolean)
+    response_id = Column(Integer, ForeignKey('responses.id'))
